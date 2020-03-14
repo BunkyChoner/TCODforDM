41cc396bf0c41be515c99d370de81900c36172f9
BunkyChoner <62163039+BunkyChoner@users.noreply.github.com>
2020-03-14 11:16:30 -0500

    Add files via upload

---
+++ components/fighter.py
@@ -0,0 +1,46 @@
+import libtcodpy as libtcod
+
+from game_messages import Message
+
+from random import randint
+
+
+class Fighter:
+    def __init__(self, hp, defense, power):
+        self.max_hp = hp
+        self.hp = hp
+        self.defense = defense
+        self.power = power
+
+    def take_damage(self, amount):
+        results = []
+
+        self.hp -= amount
+
+        if self.hp <= 0:
+            results.append({'dead': self.owner})
+
+        return results
+    
+    def heal(self, amount):
+        self.hp += amount
+
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+    def str_increase(self, amount):
+        self.power += amount
+
+    def attack(self, target):
+        results = []
+
+        damage = randint(self.power - 3, self.power) - target.fighter.defense
+
+        if damage > 0:
+            results.append({'message': Message('{0} attacks {1} for {2} hit points.'.format(
+                self.owner.name.capitalize(), target.name, str(damage)), libtcod.white)})
+            results.extend(target.fighter.take_damage(damage))
+        else:
+            results.append({'message': Message('{0} attacks {1} but does no damage.'.format(
+                self.owner.name.capitalize(), target.name), libtcod.white)})
+
+        return results
